0204f6622eea01010b9dc5ad7728cdbd9c466725
wangdaoo <wangdaoo@yeah.net>
2021-09-29 22:27:07 +0800

    feat: 测试钉钉提交反馈 :rocket:

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -7,11 +7,11 @@ export default (appInfo: EggAppInfo) => {
 
   config.orm = {
     type: 'mysql',
-    host: process.env.MYSQL_HOST,
-    port: parseInt(process.env.MYSQL_PORT),
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST || '127.0.0.1',
+    port: parseInt(process.env.MYSQL_PORT || '3306'),
+    username: process.env.MYSQL_USER || 'test',
+    password: process.env.MYSQL_PASSWORD || '123456',
+    database: process.env.MYSQL_DATABASE || 'cool',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: false,
     // 打印日志

f85f7e00bb1cc1b17b1c7e9baeddeaf97177d42d
山人自有妙计 <wangdaoo@yeah.net>
2021-10-18 15:46:18 +0800

    feat: 服务器容器运行瑕疵 :tada:

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -7,10 +7,10 @@ export default (appInfo: EggAppInfo) => {
 
   config.orm = {
     type: 'mysql',
-    host: process.env.MYSQL_HOST || '1.117.74.85',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
     port: parseInt(process.env.MYSQL_PORT || '3306'),
     username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'Mars418677-+.',
+    password: process.env.MYSQL_PASSWORD || '123456',
     database: process.env.MYSQL_DATABASE || 'cool',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: false,
